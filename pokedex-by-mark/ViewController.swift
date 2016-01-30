16d8b05448d7553e1e57d4e757c053a8df6eaee9
Mark Kramer <markkramer@marks-macbook-pro.local>
2016-01-30 16:59:20 -0500

    Initial Commit

---
+++ pokedex-by-mark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-mark
+//
+//  Created by Mark Kramer on 1/30/16.
+//  Copyright © 2016 Mark Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
